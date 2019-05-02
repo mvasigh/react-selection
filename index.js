661d332a2f203051203263800cc0eb1fdb6963ab
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-05-02 13:23:17 -0500

    add basic test app

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM, { render } from 'react-dom';
+import { render } from 'react-dom';
 import { Markup } from './src/lib';
+import './index.css';
+
+const IMAGE_SRC = 'https://cdn.spacetelescope.org/archives/images/wallpaper2/heic1509a.jpg';
 
 const App = props => {
-  return <Markup />
-}
+  return <Markup src={IMAGE_SRC} />;
+};
 
 render(<App />, document.getElementById('app'));
